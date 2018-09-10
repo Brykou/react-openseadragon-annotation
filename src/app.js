7448535a6fc4c2d52a7669dbc6deeec5d68868f0
Manuel Vila <mvila@3base.com>
2018-09-11 08:32:32 +0900

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import OpenSeadragon from '@medmain/react-openseadragon';
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{width: '100vw', height: '100vh'}}>
+        <OpenSeadragon
+          tileSources="https://image-server.images.test.medmain.com/iiif/2/pidport%2Fimages%2Fpyramidal%2Fcjlvtt4ph000201qllnygn141.tiff"
+          showNavigationControl={false}
+          style={{width: '100%', height: '100%'}}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
