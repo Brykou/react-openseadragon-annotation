e2c6bb9724e307adc190ed5d0a0c54232a9f9f7e
Brice <brykou@gmail.com>
2018-09-14 20:56:48 +0200

    Add prettier
    Add prettier and prettify JS files.

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import OpenSeadragon from './react-openseadragon';
 
 class App extends Component {
   render() {
     return (
-      <div style={{width: '100vw', height: '100vh'}}>
+      <div style={{ width: '100vw', height: '100vh' }}>
         <OpenSeadragon
           tileSources="https://image-server.images.test.medmain.com/iiif/2/pidport%2Fimages%2Fpyramidal%2Fcjlvtt4ph000201qllnygn141.tiff"
           showNavigationControl={false}
-          style={{width: '100%', height: '100%'}}
+          style={{ width: '100%', height: '100%' }}
         />
       </div>
     );
