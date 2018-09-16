b8d80014ca823e1d3f16f6ad0af24348a9b2d819
Brice <brykou@gmail.com>
2018-09-16 19:04:19 +0200

    Create Overlay component
    Create Overlay component, taking an array of shapes (a shape is an array of
    points), and rendering Annotations.

---
+++ src/components/Overlay.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Annotation from './Annotation';
+
+export default class Overlay extends React.Component {
+  static propTypes = {
+    shapeList: PropTypes.arrayOf(
+      PropTypes.arrayOf(
+        PropTypes.shape({
+          x: PropTypes.number.isRequired,
+          y: PropTypes.number.isRequired
+        }).isRequired
+      ).isRequired
+    ).isRequired,
+    currentShape: PropTypes.arrayOf(
+      PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired
+      }).isRequired
+    ).isRequired
+  };
+
+  render() {
+    const { shapeList, currentShape } = this.props;
+    return (
+      <svg preserveAspectRatio="none" viewBox={`0 0 ${window.innerWidth} ${window.innerHeight}`} wdth="100%" height="100%">
+        {
+          shapeList.map((shape, index) => {
+            return <Annotation key={index} points={shape} />;
+          })
+        }
+        { currentShape.length > 0 ? <Annotation points={currentShape} /> : null }
+      </svg>
+    );
+  }
+}
