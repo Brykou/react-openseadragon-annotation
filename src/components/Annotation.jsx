30e63ba37de6aa79e2d864da9aec0c44a2163c37
Brice <brykou@gmail.com>
2018-09-16 19:02:32 +0200

    Create Annotation component
    Create Annotation component which renders an array of points as a SVG
    polygon.

---
+++ src/components/Annotation.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { reduce } from 'lodash';
+
+export default class Annotation extends React.Component {
+  static propTypes = {
+    points: PropTypes.arrayOf(
+      PropTypes.shape({
+        x: PropTypes.number.isRequired,
+        y: PropTypes.number.isRequired
+      }).isRequired
+    ).isRequired
+  };
+
+  render() {
+    const { points } = this.props;
+    const polygonPoints = reduce(points, (sum, point) => {
+      return sum + `${point.x},${point.y} `;
+    }, '');
+    
+    return <polygon fill="red" fillOpacity="0.3" stroke="red" strokeWidth="2px" points={polygonPoints} />;
+  }
+}
